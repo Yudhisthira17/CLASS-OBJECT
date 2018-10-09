8b28c5375534cbc2d22604d8ed74551dfbb1af15
Yudhisthira17 <43755811+Yudhisthira17@users.noreply.github.com>
2018-10-09 23:16:16 +0700

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,11 @@
+
+public class stack {
+	//Attributtes
+		String[] value = new String[100];
+		int top;
+		
+		//Method setter
+			String setvalueAt(int position, String value){
+				return value;
+			}
+}
